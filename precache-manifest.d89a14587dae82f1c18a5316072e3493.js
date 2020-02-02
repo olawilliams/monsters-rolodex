2b0677827097dc7808c9f6c8d9d60b2c6377fa8f
Your Name <you@example.com>
2020-02-02 16:48:35 +0100

    Updates

---
+++ precache-manifest.d89a14587dae82f1c18a5316072e3493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc21757d189b51208a504198bafbccf8",
+    "url": "/olailliams/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "97919d6964afc6739916",
+    "url": "/olailliams/monsters-rolodex/static/css/main.1d35d35d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/olailliams/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/olailliams/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97919d6964afc6739916",
+    "url": "/olailliams/monsters-rolodex/static/js/main.cb6282d3.chunk.js"
+  },
+  {
+    "revision": "0d50fd02c9cebf249afd",
+    "url": "/olailliams/monsters-rolodex/static/js/runtime-main.d94bde98.js"
+  }
+]);
